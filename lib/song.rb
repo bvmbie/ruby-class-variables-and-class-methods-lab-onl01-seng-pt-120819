72b29a41b75ff059cf1db4faa8844344de88d3b3
Henna Vahatiitto <hnnamv@gmail.com>
2020-01-10 17:00:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
     @@genres.each do |genre|
       if genre_count[genre] 
         genre_count[genre] += 1
-      else #if it's a new genre
+      else 
         genre_count[genre] = 1
       end
     end
@@ -44,7 +44,7 @@ class Song
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count[artist] #if the artist is already in the hash
+      if artist_count[artist] 
         artist_count[artist] += 1
       else
         artist_count[artist] = 1
